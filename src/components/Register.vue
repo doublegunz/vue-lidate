e43ef0cdd1b5a99665b5a927113f97ae020ae972
Gun Gun Priatna <gungunpriatna02@gmail.com>
2018-10-19 07:03:48 +0700

    add vuelidate-error-extractor

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,24 +1,20 @@
 <template>
     <form @submit.prevent="submit">
-        <div class="form-group">
-            <label for="">Nama Lengkap</label>
+        <form-group :validator="$v.name" label="Nama Lengkap">
             <input type="text" v-model="name" class="form-control" :class="{'is-invalid': $v.name.$error}">
-        </div>
+        </form-group>
 
-        <div class="form-group">
-            <label for="">Email</label>
+        <form-group :validator="$v.email" label="Email">
             <input type="email" v-model="email" class="form-control" :class="{'is-invalid': $v.email.$error}">
-        </div>
+        </form-group>
 
-        <div class="form-group">
-            <label for="">No Telepon</label>
+        <form-group :validator="$v.phone" label="No Telepon">
             <input type="text" v-model="phone" class="form-control" :class="{'is-invalid': $v.phone.$error}">
-        </div>
+        </form-group>
 
-        <div class="form-group">
-            <label for="">Password</label>
+        <form-group :validator="$v.password" label="Password">
             <input type="password" v-model="password" class="form-control" :class="{'is-invalid': $v.password.$error}">
-        </div>
+        </form-group>
 
         <div class="form-group">
             <button class="btn btn-primary btn-sm">Daftar</button>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuelidate from 'vuelidate'
+import VuelidateErrorExtractor, { templates } from "vuelidate-error-extractor";
 
 Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
+
+//define error message
+Vue.use(VuelidateErrorExtractor, {
+  i18n: false,
+  messages: {
+    required: "{attribute} is required!",
+    notGmail: "{attribute} must gmail address",
+    notPhone: "{attribute} must phone number Indonesia format",
+    email: "{attribute} is not a valid Email address.",
+    isEmailAvailable: "{attribute} is not available. Must be at least 10 characters long."
+  }
+});
+
+Vue.component("form-group", templates.singleErrorExtractor.foundation6); //membuat global component dengan nama form-group
 
 new Vue({
   render: h => h(App)
